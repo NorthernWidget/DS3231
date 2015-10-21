436631e00e820c129fc448ecd29b2709a6433af0
Andy Wickert <andy@northernwidget.com>
2015-10-21 12:22:49 -0500

    2's complement in temperature reading to work for negative temperatures

--- DS3231.cpp
+++ DS3231.cpp
@@ -349,14 +349,31 @@ void DS3231::setClockMode(bool h12) {
 float DS3231::getTemperature() {
 	// Checks the internal thermometer on the DS3231 and returns the 
 	// temperature as a floating-point value.
-	byte temp;
-	Wire.beginTransmission(CLOCK_ADDRESS);
-	Wire.write(0x11);
-	Wire.endTransmission();
 
-	Wire.requestFrom(CLOCK_ADDRESS, 2);
-	temp = Wire.read();	// Here's the MSB
-	return float(temp) + 0.25*(Wire.read()>>6);
+  // Updated / modified a tiny bit from "Coding Badly" and "Tri-Again"
+  // http://forum.arduino.cc/index.php/topic,22301.0.html
+  
+  byte tMSB, tLSB;
+  float temp3231;
+  
+  // temp registers (11h-12h) get updated automatically every 64s
+  Wire.beginTransmission(CLOCK_ADDRESS);
+  Wire.write(0x11);
+  Wire.endTransmission();
+  Wire.requestFrom(CLOCK_ADDRESS, 2);
+
+  // Should I do more "if available" checks here?
+  if(Wire.available()) {
+    tMSB = Wire.read(); //2's complement int portion
+    tLSB = Wire.read(); //fraction portion
+
+    temp3231 = ((((short)tMSB << 8) | (short)tLSB) >> 6) / 4.0;
+  }
+  else {
+    temp3231 = -9999; // Some obvious error value
+  }
+   
+  return temp3231;
 }
 
 void DS3231::getA1Time(byte& A1Day, byte& A1Hour, byte& A1Minute, byte& A1Second, byte& AlarmBits, bool& A1Dy, bool& A1h12, bool& A1PM) {
