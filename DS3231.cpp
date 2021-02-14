d99c91faef060cd21c815732b0060f3bcb04b755
Andy Wickert <andy@northernwidget.com>
2021-02-14 00:08:59 -0600

    nanf does not work; return error temperature to -9999

--- DS3231.cpp
+++ DS3231.cpp
@@ -411,7 +411,7 @@ float DS3231::getTemperature() {
     temp3231 = ( (float)itemp / 256.0 );              // Scale and return
   }
   else {
-    temp3231 = nanf(""); // Not a number
+    temp3231 = -9999; // Impossible temperature; error value
   }
    
   return temp3231;
